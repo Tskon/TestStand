a2f49b97d8d361f041950808f14e394eec2431af
tskon <beatxd@gmail.com>
2018-04-11 13:18:19 +0300

    error commit v2

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 window.addEventListener('load', () => {
-
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus consectetur cupiditate debitis deleniti, error facilis fugit obcaecati odit officiis pariatur possimus quas qui repellat reprehenderit sequi sit voluptas. Consequuntur, quo?
 });
