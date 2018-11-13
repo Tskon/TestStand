073f8d951c245f21b1697491e7be2b1a78a3bdf9
Tskon <tsekhmeister.k@gmail.com>
2018-11-13 09:05:39 +0300

    few test changes

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-window.addEventListener('load', () => {
-  console.log('hello friends!;;;');
-  console.log('whats ups');
-  console.log('glad to see u');
-});
+const val1 = +prompt("Введите число");
+const pow1 = +prompt("Введите степень");
+
+function power(val, pow) {
+    if (pow === 0) {
+        return 1;
+    }
+    return val * power(val, pow - 1);
+}
+
+alert(power(val1, pow1));
