23dd3eb108d1912aa435c48e51a8b63f22f77855
tskon <tsekhmeister.k@gmail.com>
2019-11-23 18:32:12 +0300

    check closure

--- main.js
+++ main.js
@@ -1,14 +1,8 @@
-const findLatestWeight = function(weights) {
-    if (weights.length === 1) return weights[0]
-
-    const maxVal = Math.max.apply(Math, weights)
-    let filteredArr = weights.filter(num => num !== maxVal)
-    const filteredMaxVal = Math.max.apply(Math, weights)
-    const filteredMaxValIndex = weights.indexOf(filteredMaxVal)
-
-    filteredArr[filteredMaxValIndex] = maxVal - filteredMaxVal
-
-    return findLatestWeight(filteredArr)
+function getFunc() {
+    let test = 4
+    return function () {
+        console.log(test)
+    }
 }
 
-console.log(findLatestWeight([2,7,4,1,8,1]))
+const testF = getFunc()
